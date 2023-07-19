097fd9f64de81346161b3f1149c80ee835eae746
RuthSamm <newrutha3@gmail.com>
2023-07-19 12:57:57 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,13 +18,15 @@ int main(void)
 			{
 				putchar (i);
 				putchar(j);
-				if (i != '0' || (i == '0' && j != '9'))
+				if (i != '8' || (i == '8' && j != '9'))
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
+			j++;
 		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
